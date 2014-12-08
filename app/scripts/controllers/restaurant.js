353ac8b6b91721d1fe4f84560e9abdb2393756ca
jreed91 <jreed91@gmail.com>
2014-12-08 11:06:10 -0600

    Got communication working with 4square

--- app/scripts/controllers/restaurant.js
+++ app/scripts/controllers/restaurant.js
@@ -1,18 +1,28 @@
 'use strict';
 
 app.controller('restaurantCtrl', function($scope, $location, Restaurant, geolocation, Foursquare){
-	
-	$scope.coords = geolocation.getLocation().then(function(data){
-      return {lat:data.coords.latitude, long:data.coords.longitude};
+
+
+  	geolocation.getLocation().then(function(data){
+      $scope.coords = data.coords.latitude + ',' + data.coords.longitude;
     });
 
 
-	// Foursquare.venues()
-	// 	.success(function(data, status, headers){
-	// 		$scope.venues = data.data;
-	// 	});
-	
 
+
+	Foursquare.venues($scope.coords)
+		.success(function(data, status, headers){
+			if (angular.isArray(data.Venues.Venue)) {
+				$scope.venues = data.Venues.Venue;
+			}
+			else {
+				$scope.venues = [data.Venues.Venue];
+			}
+		});
+	
+	angular.forEach($scope.venues, function(venue) {
+	    console.log(venue);
+	});
 
 	$scope.restaurants = Restaurant.all;
 	$scope.restaurant = {name: '', address: '', city: '', state: ''};
--- app/scripts/services/foursquare.js
+++ app/scripts/services/foursquare.js
@@ -1,12 +1,16 @@
 'use strict';
 
-app.factory('Foursquare', ['$http', function($http){
+app.factory('Foursquare', ['$http', '$q', function($http){
+	
 	var venues = [];
+
+	var client_id = 'JZE1R0YPBSAH0STHQ3JI4SLNRSF21VPEEOX04FLHTGIMUPYP';
+	var client_secret = 'ZBV2BPGHYDBVACNSAVDQDXL0HR1AOLBSBIKJR2WESQVLALDM';
 
 	var request = function(location) {
 		return $http({
 			method: 'JSONP',
-			url: 'https://api.foursquare.com/v2/venues/search?ll=' + location
+			url: 'https://api.foursquare.com/v2/venues/search?client_id=' + client_id +'&client_secret=' + client_secret +'&v=20130815&&ll=41.4553565,-91.0239029'
 		});
 	}
 	return {
