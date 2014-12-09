8de9740f39edfc7f195db6e136ce5144fb08d1d5
jreed91 <jreed91@gmail.com>
2014-12-09 11:57:57 -0600

    Added Venue Detail Page

--- app/scripts/controllers/restaurantview.js
+++ app/scripts/controllers/restaurantview.js
@@ -1,5 +1,11 @@
 'use strict';
 
 app.controller('RestaurantViewCtrl', function ($scope, $routeParams, Foursquare) {
-	$scope.venue = Foursquare.get($routeParams.venueId);
+	$scope.venue = [];
+
+	Foursquare.getVenue($routeParams.venueId)
+		.then(function(data){
+		$scope.venue = data;
+		}, function(data){
+	});
 });
--- app/scripts/services/foursquare.js
+++ app/scripts/services/foursquare.js
@@ -4,32 +4,36 @@ app.factory('Foursquare', function($http, $q){
 	
 	var venues = [];
 	var service = {}
-	var baseUrl = 'https://api.foursquare.com/v2/venues/search?client_id='
+	var baseUrl = 'https://api.foursquare.com/v2/venues/'
 	var client_id = 'JZE1R0YPBSAH0STHQ3JI4SLNRSF21VPEEOX04FLHTGIMUPYP';
 	var client_secret = 'ZBV2BPGHYDBVACNSAVDQDXL0HR1AOLBSBIKJR2WESQVLALDM';
 	var _coords = '';
 	var _finalUrl = '';
 
-	var makeUrl = function() {
-		_finalUrl = baseUrl + client_id + '&client_secret=' + client_secret + '&ll=' + _coords + '&v=20130815&intent=browse&radius=800';
-		return _finalUrl;
-	}
 
 	service.setCoords = function(coords) {
 		console.log(coords);
 		_coords = coords;
 	}
 
-	service.setVenue = function(venueId) {
-		makeUrl
-	}
-
-	service.getVenues = function (){
-		makeUrl();
+	service.getVenue = function(venueId) {
 		var deferred = $q.defer();
 		$http({
 			method: 'GET',
-			url: _finalUrl
+			url: baseUrl + venueId +'?v=20130815&client_id=' + client_id + '&client_secret=' + client_secret
+		}).success(function(data){
+			deferred.resolve(data);
+		}).error(function(){
+			deferred.reject('Error')
+		})
+		return deferred.promise;
+	}
+
+	service.getVenues = function (){
+		var deferred = $q.defer();
+		$http({
+			method: 'GET',
+			url: baseUrl + 'search?client_id=' + client_id + '&client_secret=' + client_secret + '&ll=' + _coords + '&v=20130815&intent=browse&radius=800'
 		}).success(function(data){
 			deferred.resolve(data);
 		}).error(function(){
